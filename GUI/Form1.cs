e2d30f1f1e5ffeaf225834f4277b3f8f8cfb446b
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-10-11 20:05:41 +0700

    11/10/2023

---
+++ GUI/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = int.Parse(textBox1.Text);
+                int n2 = int.Parse(textBox2.Text);
+                int sum = n1 + n2;
+                textBox3.Text = sum.ToString();
+            } catch
+            {
+                MessageBox.Show("Dau vao khong hop le");
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI_Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Form form1 = new Form();
+            //Form form2 = new Form();
+            //form1.Text = "Form passed to Run()";
+            //form2.Text = "Second form";
+            //form1.Cursor = Cursors.WaitCursor;
+            //Application.Run(form1);
+            //form2.Show();
+            //MessageBox.Show("Application.Run() has returned control back to Main.Bye,bye!", "TwoForms");
+            Form f = new Form();
+            f.Load += new EventHandler(f_Load);
+            Application.Run(f);
+        }
+        private static void f_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
