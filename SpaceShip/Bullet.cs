2957df173dfdb26029dd4f57af7c10d76a2b3714
caovanhoang63 <22520457@gm.uit.edu.vn>
2023-12-12 19:12:30 +0700

    SpaceShip

---
+++ SpaceShip/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace SpaceShip
+{
+    internal class Bullet : FlyingObject
+    {
+        
+        
+        public Bullet(int x, int y) : base(x, y)
+        {
+            _width = 20;
+            _height = 25;
+            _image = new Bitmap("./../../Assets/Image/LaserBullet.png");
+            _image = new Bitmap(_image, _width, _height);
+            _rectangle = new Rectangle(_x,_y, _width, _height);
+            _speed = 10;
+        }
+        
+
+        public override void Explosion(Graphics g)
+        {
+            
+            return;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.DrawImage(_image, _x, _y);
+            //graphics.DrawRectangle(Pens.Brown, _rectangle);
+        }
+        
+        public override void Move()
+        {
+            _y -= _speed;
+            _rectangle.Y = _y;
+        }
+        
+    }
+}
---
+++ SpaceShip/Keyboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SpaceShip
+{
+    public static class Keyboard
+    {
+        private static readonly HashSet<Keys> keys = new HashSet<Keys>();
+
+        public static void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+            if (keys.Contains(e.KeyCode) == false)
+            {
+                keys.Add(e.KeyCode);
+            }
+        }
+
+        public static void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (keys.Contains(e.KeyCode))
+            {
+                keys.Remove(e.KeyCode);
+            }
+        }
+
+        public static bool IsKeyDown(Keys key)
+        {
+            return keys.Contains(key);
+        }
+    }
+}
